a02bd90e777126fac4eaf24414167686f9c456cc
Deathofmatrix <114542867+Deathofmatrix@users.noreply.github.com>
2022-09-28 11:59:22 +1000

    Started week 2 activities
    Favourite game, hours played, cost of game

---
+++ Assets/Scripts/WeekTwoActivities.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CharlieHarrop
+{
+    public class WeekTwoActivities : MonoBehaviour
+    {
+        private string favouriteGame = "Outer Wilds";
+        private int timePlayed = 58;
+        private float costOfGame = 23.36f;
+   
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("My favourite game is " + favouriteGame + " I have played it for " + timePlayed + " hours, and it cost me $" + costOfGame + ". Therefore, my value of dollars per hour is $" + costOfGame / timePlayed + ".");
+        
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+
+}
+
